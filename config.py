e0b7a86727058f507f459601b05c7d3ac4fe8e0c
slayer123700 <meerutuniversity996@gmail.com>
2025-06-20 18:51:31 +0530

    sʜɪɢᴀʀᴀkɪ ᴛᴏᴍᴜʀᴀ

---
+++ config.py
@@ -0,0 +1,56 @@
+
+class config:
+    
+    #Client
+    API_ID = 26249286
+    API_HASH = "4e3bf0b014fda4ac752e8f4ab854279b"
+    BOT_TOKEN = "7830572812:AAFljrsYesFuYfg4skV7KvGuecMexT2X6w4"
+    BOT_NAME = "GODxMUSIC"
+    BOT_USERNAME = "GODxMUSIC_probot"
+    BOT_ID = 7830572812
+    WORKERS = 30
+    MAX_MESSAGE_CACHE_SIZE = 100
+    MAX_CONCURRENT_TRANSMISSIONS = 10
+    
+     
+    #Git
+    GIT_USERNAME = "Slayer123700"
+    GIT_URL_WITH_TOKEN = "https://Your_Git_Token@github.com/Your_Git_Username/Yumeko.git" 
+
+
+    #Info
+    BOT_VERSION = "x"
+    OWNER_ID = 6018803920
+    OWNER_USERNAME = "slayer1237"
+    SUPPORT_CHAT = -1002333824980
+    SUPPORT_CHAT_USERNAME = "Domihoes"
+    SUPPORT_CHAT_LINK = "https://t.me/Domihoes"
+    LOG_CHANNEL = -1002392274240
+    ERROR_LOG_CHANNEL = -1002392274240
+    DOWNLOAD_LOCATION = "./downloads"
+    COMMAND_PREFIXES = ["/" , "!" , "." , "#" , "$" , "%" , "&" , "?"] 
+    CMD_STARTERS = "/.!&#%$"
+    STATS_IMG_URL = "https://i.ibb.co/gFg5XstQ/photo-2025-05-24-04-00-24-7507857220025974820.jpg"
+    START_IMG_URL = "https://i.ibb.co/gFg5XstQ/photo-2025-05-24-04-00-24-7507857220025974820.jpg"
+    HELP_IMG_URL = "https://files.catbox.moe/bggrlh.jpg"
+    ALIVE_IMG_URL = "https://files.catbox.moe/f3yuiy.jpg"
+    
+    
+    #Database
+    MONGODB_URI = "mongodb+srv://musicbotxd:musicbotxd@cluster0.6thyk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" # Use Local Hosted Or Atlas !!
+    DATABASE_NAME = "musicbot"
+
+    #API
+    ARQ_API_KEY = "RLWCED-WZASYO-AWOLTB-ITBWTP-ARQ"
+    ARQ_API_URL = "arq.hamker.dev"
+    CRICKET_API_URL = "https://sugoi-api.vercel.app/cricket"
+    FOOTBALL_API_URL = "https://sugoi-api.vercel.app/football"
+    BINGSEARCH_URL = "https://sugoi-api.vercel.app/search"
+    NEWS_URL = "https://sugoi-api.vercel.app/news?keyword={}"
+    shayri_api_url = "https://hindi-quotes.vercel.app/random"
+    BASE_URL = "https://api.waifu.pics"
+    Movie_Api = "d6b8dd51c4e80b5888e2587d780300fc"
+    Movie_RAC = "AQFy0zIAeTrqXAyN24IN6WCSafrxbO81MFls0ObprFIFNDUixJJjgGITAdkghmp2aiWnhMnY-VbLFph-jaMKIEfsw7shXYiB6-qZOVlV0LKm3Imfd8GZ-AWzmj7O_v1VfcwtM8NeLkeJlGbaix6JpQeP_1ZefphDuxdCzrqzEOmaGeWvPj1cs9sjzYgTyAoehubC5XnbVuRU-7Xu6kmPssSQMYMABAZB35EGu47c-RAvyx43MkLKXzBQGKfU2Lx-YElS1U6jCwBTn4z6LYcct7ZT78CT7G-XK-P3PyLylYcGR0j90lGIzud7mJZm8ZNIHc-5NTlG-mxmK3l0MHjNMgW6bM_a9AAAAAGxDHVkAA"
+    Pokedex = "https://sugoi-api.vercel.app/pokemon?name={name_or_id}"
+    OPENAI_KEY = "xx" #Get From Open Ai's Website
+    LYRICS_GENIUS_TOKEN = "x-x" #Get From Genius.com
